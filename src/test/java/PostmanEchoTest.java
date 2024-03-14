c9a4719316f3ab14d78e5b3c96e11961c3202eef
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-03-14 16:19:38 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+            void shouldReturnSendData() {
+        var text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text) // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text))
+        ;
+    }
+
+}
